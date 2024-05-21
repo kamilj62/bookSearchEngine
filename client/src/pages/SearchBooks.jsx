610650ddf535cdaad6662469c6634ee71aa01df7
Joseph Kamil <kamilj@umich.edu>
2024-05-21 08:48:46 -0700

    fixd searchBooks

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -79,7 +79,7 @@ const SearchBooks = () => {
   
       try {
         const { data } = await saveBook({
-          variables: { bookToSave },
+          variables: { ...bookToSave },
         });
       
       if (!data) {
